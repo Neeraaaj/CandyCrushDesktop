dc83da9dfd8e340633919ff602c7e18880b39c6f
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2023-11-21 20:20:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/app" element={<App />} />
+      <Route path="/" element={<App />} />
     </Routes>
   </Router>
 )
