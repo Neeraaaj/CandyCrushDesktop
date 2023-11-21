90ce2406010a8fbd74a3c74358070dcfb54b2fea
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2023-11-21 19:18:35 +0530

    added files

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Login from './login.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/app" element={<App />} />
+    </Routes>
+  </Router>
+)
